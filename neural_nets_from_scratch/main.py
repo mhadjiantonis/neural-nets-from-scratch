865d83b4e27ef00f540807f355c5e3ca44ed10a3
Marios Hadjiantonis <marios.hadjiantonis@truecaller.com>
2025-09-03 13:51:45 +0200

    Add classes for activation functions

--- neural_nets_from_scratch/main.py
+++ neural_nets_from_scratch/main.py
@@ -1,27 +1,68 @@
+from abc import ABC, abstractmethod
+from typing import Callable
+
 import numpy
 
 
+class ActivationFunction(ABC):
+
+    @abstractmethod
+    def func(self, a: numpy.ndarray) -> numpy.ndarray:
+        pass
+
+    @abstractmethod
+    def derivative(self, a: numpy.ndarray) -> numpy.ndarray:
+        pass
+
+
+class ReLUActivation(ActivationFunction):
+
+    def func(self, a: numpy.ndarray) -> numpy.ndarray:
+        return a.clip(min=0.0)
+
+    def derivative(self, a: numpy.ndarray) -> numpy.ndarray:
+        return numpy.where(a > 0.0, 1.0, 0.0)
+
+
+class SoftMaxActivation(ActivationFunction):
+
+    def func(self, a: numpy.ndarray) -> numpy.ndarray:
+        exp = numpy.exp(a)
+        return exp / exp.sum()
+
+    def derivative(self, a: numpy.ndarray) -> numpy.ndarray:
+        func = self.func(a)
+        return func - numpy.pow(func, 2)
+
+
 class DenseLayer:
+    input_size: int
+    output_size: int
+    activation_function: ActivationFunction
+    weights: numpy.ndarray
+    biases: numpy.ndarray
+    input: numpy.ndarray
+    output: numpy.ndarray
+    derivative_output: numpy.ndarray
+
     def __init__(
         self,
         input_size: int,
         output_size: int,
-        activation_function,
-        activation_function_derivative,
+        activation_function: ActivationFunction,
     ):
         self.input_size = input_size
         self.output_size = output_size
         self.activation_function = activation_function
-        self.activation_function_derivative = activation_function_derivative
         self.weights = numpy.random.rand(input_size, output_size) - 0.5
         self.biases = numpy.random.rand(output_size) - 0.5
 
     def forward(self, input: numpy.ndarray) -> numpy.ndarray:
         self.input = input
         linear = input.dot(self.weights) + self.biases
-        output = self.activation_function(linear)
+        output = self.activation_function.func(linear)
         self.output = output
-        self.derivative_output = self.activation_function_derivative(linear)
+        self.derivative_output = self.activation_function.derivative(linear)
         return output
 
     def backward(self, errors: numpy.ndarray) -> numpy.ndarray:
@@ -35,6 +76,8 @@ class DenseLayer:
 
 
 class SequentialModel:
+    layers: list[DenseLayer]
+
     def __init__(self, layers: list[DenseLayer]):
         self.layers: list[DenseLayer] = []
         for layer in layers:
@@ -75,30 +118,10 @@ if __name__ == "__main__":
 
     model = SequentialModel(
         [
-            DenseLayer(
-                3,
-                40,
-                numpy.vectorize(lambda x: max(0.0, x)),
-                numpy.vectorize(lambda x: 1.0 if x >= 0.0 else 0.0),
-            ),
-            DenseLayer(
-                40,
-                10,
-                numpy.vectorize(lambda x: max(0.0, x)),
-                numpy.vectorize(lambda x: 1.0 if x >= 0.0 else 0.0),
-            ),
-            DenseLayer(
-                10,
-                5,
-                numpy.vectorize(lambda x: max(0.0, x)),
-                numpy.vectorize(lambda x: 1.0 if x >= 0.0 else 0.0),
-            ),
-            DenseLayer(
-                5,
-                1,
-                numpy.vectorize(lambda x: max(0.0, x)),
-                numpy.vectorize(lambda x: 1.0 if x >= 0.0 else 0.0),
-            ),
+            DenseLayer(3, 40, ReLUActivation()),
+            DenseLayer(40, 10, ReLUActivation()),
+            DenseLayer(10, 5, ReLUActivation()),
+            DenseLayer(5, 1, SoftMaxActivation()),
         ]
     )
     dataset_size = 10000
@@ -108,6 +131,6 @@ if __name__ == "__main__":
 
     external_errors = numpy.random.rand(dataset_size, 1)
     model.backward(external_errors.T)
-    print(model.get_weights())
+    # print(model.get_weights())
     model.update_weights(0.1)
-    print(model.get_weights())
+    # print(model.get_weights())
