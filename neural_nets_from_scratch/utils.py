d7bc2c767687a36abea1a76af255c9920c91e77e
Marios Hadjiantonis <marios.hadjiantonis@truecaller.com>
2025-09-05 18:46:51 +0200

    Improve annotations

--- neural_nets_from_scratch/utils.py
+++ neural_nets_from_scratch/utils.py
@@ -1,8 +1,9 @@
 import numpy
 
+
 def shuffle_data(
-    X: numpy.ndarray, Y: numpy.ndarray
-) -> tuple[numpy.ndarray, numpy.ndarray]:
+    X: numpy.typing.NDArray[numpy.float64], Y: numpy.typing.NDArray[numpy.float64]
+) -> tuple[numpy.typing.NDArray[numpy.float64], numpy.typing.NDArray[numpy.float64]]:
     data = numpy.concat((X, Y), axis=1)
     numpy.random.shuffle(data)
     X_shuffle = data[:, : X.shape[1]]
